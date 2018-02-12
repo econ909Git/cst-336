6ec2487ba6a178fc0aa9ea2b74754e41b4ffc921
John Economides <jeconomides@csumb.edu>
2018-02-12 21:46:50 +0000

    cleanup

---
+++ Examples/example.php
@@ -0,0 +1,42 @@
+<?php
+   $num1 = rand(0,10);
+   $num2 = rand(0,10);
+   $both = array( 1=> num1, 2 => num2);
+   $correct_guess = False;
+   
+ <h2> Enter two number from 0-10 </h2>
+   
+<form action ="index.php" method = "POST">
+<input type = "text" name = "number1" value=''/>
+<input type = "text" name = "number2" value=''/>
+</form>
+
+for i, 
+if(number1 > num1) 
+    print("The first number should be lower");
+        if(number2 > num2){
+            print("The second number should be lower");
+if(number1 < num1) 
+    print("The first number should be higher");
+        if(number2 > num2){
+            print("The second number should be lower");
+if
+
+}
+elif (number1 << num1) && (number2 > num2)
+?>
+<html>
+    <head>
+        <title> PHP Basic </title>
+    </head>
+    <body>
+        <?php
+            echo "<h2>" . $content * 1.4 . "</h2>"
+        
+            echo $content
+            header("Content Type'", "image/png")
+            
+        ?>
+
+    </body>
+</html>
---
+++ HW/1/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<!--
+1st HW assignment
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="uft-8" />
+        <title>Computer Science: In Every Day Life</title>
+        <style>
+            @import url("css/styles.css");
+        </style> 
+        <link href="https://fonts.googleapis.com/css?family=Rammetto+One" rel="stylesheet"> 
+    </head>
+<!-- closing head -->
+    
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Computer Science: In Every Day Life </h1>
+        </header>
+        <nav>
+            <hr width="50%" />
+            <a href="index.php">Home<a/>
+            <a href="trasport.php">Transportation</a>
+            <a href="comm.php">Communication</a>
+            <a href="games.php">Games</a>
+        </nav>
+        
+        <br /><br />
+        <main>
+            
+            <figure id="code">
+                <img src="img\code.jpg" alt="Picture of John Economides" />
+            </figure>
+            
+            
+            <div id="welcome_text">
+                 <br /><br />
+                 Hello!<br />
+                <p>Computer are in everything you use today.</p> <p>If its getting cars running to games being made, code is used in everything</p>
+                <p>Without computers we wouldn't be able to communicate so easily.</p> 
+                <p>This webpage shows some of the things computer science is improving today</p>
+                <p>I focused on the four I though were most important</p>
+                <p>Feel free to contact me:</p>
+                <p>jeconomides@csumb.edu</p>
+                </div>
+                <br /><br />
+                
+             
+                <figure id="code">
+                <img src="img\code.jpg" alt="Picture of John Economides" />
+            </figure>
+           
+            
+                <div id="quote">
+                 <br /><br />
+                  <br /><br /> 
+                <em>"Computers are getting <strong>smarter</strong> all the time.  Scientists tell us that soon they will be able to <strong>talk to us</strong>."</em>
+                
+                <br />
+                -Dave Berry
+                </div>
+            </div>    
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            <figure id="csumb">
+                <img src="img\csumb.jpg" alt="csumb logo" />
+            </figure>
+            CST 336 Internet Programming. 2018&copy: Economides <br />
+            <strong>Disclaimer:</strong>The information on this website is fictitous.  <br />
+            it is used for acadimc purposes only.
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
