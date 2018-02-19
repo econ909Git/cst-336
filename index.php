52decd923c6f7eedc408d94fedb8cf5d4d8dc2e4
John Economides <jeconomides@csumb.edu>
2018-02-19 04:04:29 +0000

    still trying to get Lab 2 link working

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <a href="/Labs/1">Lab 1</a>
-        <a href="/Labs/2">Lab 2</a>
+        <a href="/Labs/2/777">Lab 2</a>
         <a href="/HW/1">HW 1</a>
     </body>
 </html>
